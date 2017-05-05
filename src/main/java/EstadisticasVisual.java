0e3d8c664afd362d38f616dfeaa61cbe286e6f31
Julián David Devia Serna <judadese@gmail.com>
2017-05-05 14:37:17 -0500

    datos

--- src/main/java/EstadisticasVisual.java
+++ src/main/java/EstadisticasVisual.java
@@ -3,6 +3,8 @@ import java.util.Calendar;
 import java.util.Date;
 import javax.swing.JOptionPane;
 
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- src/main/java/edu/eci/labinfo/estadisticasV2/analysis/Analysis.java
+++ src/main/java/edu/eci/labinfo/estadisticasV2/analysis/Analysis.java
@@ -27,7 +27,7 @@ import java.util.logging.Logger;
  * @author Daniela Sepulveda Alzate
  */
 public class Analysis {
-    private static final HashMap<String,Integer> meses = new HashMap<String,Integer>() {{     
+    private static final HashMap<String,Integer> MESES = new HashMap<String,Integer>() {{     
        put("ene", 0);     
        put("feb", 1);     
        put("mar", 2);     
@@ -40,32 +40,31 @@ public class Analysis {
        put("oct", 9); 
        put("nov", 10); 
        put("dic", 11); 
-    }};  
-    //turnos
-    private int[][] b0;
-    //plataformas
-    private int[][] plat;
-    //redes
-    private int[][] red;
-    //ingenieria software
-    private int[][] soft;
-    //interactiva
-    private int[][] inte;
-    //multimedia
-    private int[][] multi;
+    }};
+    private static final HashMap<String,int[][]> SALONES = new HashMap<String,int[][]>() {{     
+       put("REDES", new int[8][6]);     
+       put("INFRAESTRUCTURA", new int[8][6]);     //plataformas
+       put("B-0", new int[8][6]);      //ingenieria IW
+       put("C1-205B", new int[8][6]);     
+       put("INTERACTIVA", new int[8][6]);
+       put("TURNOS", new int[8][6]); //BO (turnos)    
+    }};
     
+    private static final HashMap<String,Integer> PCXSALONES = new HashMap<String,Integer>() {{     
+       put("REDES", 12);     
+       put("INFRAESTRUCTURA",21 );     //plataformas
+       put("B-0",24 );      //ingenieria IW
+       put("C1-205B", 12);     
+       put("INTERACTIVA",7 );
+       put("TURNOS", 25); //BO (turnos)    
+    }};
+   
     public Analysis(){
-       b0 = new int[8][6];
-       plat = new int[8][6];
-       red = new int[8][6];
-       soft = new int[8][6];
-       inte = new int[8][6];
-       multi = new int[8][6];
+        
     }
     
     public void statisticWeek(int cod){
-       Conexion reservas=new ReservasConexion();
-       Conexion estadisticas=new EstadisticasConexion();
+       Conexion reservas=new ReservasConexion();       
         try {
             Connection res=reservas.connection();
             String semana="SELECT semana FROM `semanas` where id="+cod;
@@ -73,19 +72,29 @@ public class Analysis {
             ResultSet rs = stmt.executeQuery(semana);
             rs.next();
             String consulta=rs.getString(1);
-            try{
-                GregorianCalendar fechaInicioSemana=new GregorianCalendar();
-                int year=fechaInicioSemana.get(Calendar.YEAR);
-                String [] fechas=consulta.trim().split("-");
-                fechaInicioSemana=new GregorianCalendar(year,meses.get(fechas[1]),Integer.parseInt(fechas[0]),0,0);
-                GregorianCalendar fechaFinSemana=new GregorianCalendar(year,meses.get(fechas[3]),Integer.parseInt(fechas[2]),23,59);
+            GregorianCalendar fechaInicioSemana=new GregorianCalendar();
+            int year=fechaInicioSemana.get(Calendar.YEAR);
+            //Format:   ejm: 17-ene-16-ene
+            String [] fechas=consulta.trim().split("-");
+            if(fechas.length==4){
+                fechaInicioSemana=new GregorianCalendar(year,MESES.get(fechas[1]),Integer.parseInt(fechas[0]),0,0);
+                GregorianCalendar fechaFinSemana=new GregorianCalendar(year,MESES.get(fechas[3]),Integer.parseInt(fechas[2]),23,59);
                 Timestamp ini =new Timestamp(fechaInicioSemana.getTimeInMillis()) ;
-                Timestamp fin= new Timestamp(fechaFinSemana.getTimeInMillis());  
-                Connection esta=estadisticas.connection();
-                Statement stmtk=esta.createStatement();
-            }catch(Exception e){
+                Timestamp fin= new Timestamp(fechaFinSemana.getTimeInMillis());
+                controlStatistics(ini,fin);
+                String reserva="SELECT * FROM  `reservas` WHERE semana =  '"+consulta+"'";
+                stmt=res.prepareStatement(reserva);
+                rs = stmt.executeQuery(semana);                
+                while(rs.next()){
+                   for (int i = 1; i < 7; i++) {
+                       
+                    
+                    
+                    }
+                }
                 
-            } 
+            }
+
             
         } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException ex) {
             Logger.getLogger(Analysis.class.getName()).log(Level.SEVERE, null, ex);
@@ -95,14 +104,33 @@ public class Analysis {
         
     }
     
-     public void statistic(int ini, int fin){
+     public void statistics(int ini, int fin){
+         for (int i = ini; i < fin+1; i++) {
+             statisticWeek(i);
+         }
         
+    }
+     
+    private void controlStatistics(Timestamp ini,Timestamp fin){
+        Conexion estadisticas=new EstadisticasConexion();
+        Connection esta;
+        try {
+            esta = estadisticas.connection();
+            Statement stmtk=esta.createStatement();
+            String consulta="SELECT * FROM `datos` "
+                    + "WHERE logon BETWEEN '2017-04-29 09:11:56' AND '2017-05-29 09:11:56'"
+                    + "AND logoff BETWEEN '2017-04-29 09:11:56' AND '2017-05-29 09:11:56'";
+            
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Analysis.class.getName()).log(Level.SEVERE, null, ex);
+            Log.anotar(ex);
+        }
         
     }
     
 
-    public int[][] getB0() {
-        return b0;
+    /*public int[][] getB0() {
+        return salones.get("B-0");
     }
 
     public int[][] getPlat() {
@@ -123,7 +151,7 @@ public class Analysis {
 
     public int[][] getMulti() {
         return multi;
-    }
+    }*/
     
    
     
